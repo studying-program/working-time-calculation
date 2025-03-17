4726cd7ac79571405c66a9863406ecd30f5430c0
rakutech! <82389956+studying-program@users.noreply.github.com>
2025-03-17 11:27:02 +0000

    add

--- flask-template/app.py
+++ flask-template/app.py
@@ -16,14 +16,19 @@ def index():
     if request.method == 'POST':
         try:
             hours1, minutes1 = parse_time(request.form['time1'])
-            hours2, minutes2 = parse_time(request.form['time2'])
-            total_minutes = (hours1 * 60 + minutes1) + (hours2 * 60 + minutes2)
+            if 'time2' in request.form:
+                hours2, minutes2 = parse_time(request.form['time2'])
+                total_minutes = (hours1 * 60 + minutes1) + (hours2 * 60 + minutes2)
+            else:
+                total_minutes = (hours1 * 60 + minutes1)
+
             total_hours = total_minutes // 60
             total_minutes = total_minutes % 60
             total_time = f"{total_hours:02}:{total_minutes:02}"
         except ValueError:
             total_time = ". Please enter time in hhmm format."
-    return render_template('index.html', total_time=total_time)
+    
+    return render_template('index.html', total_time=total_time, background_image="static/background.jpg")
 
 if __name__ == '__main__':
     app.run(debug=True)
